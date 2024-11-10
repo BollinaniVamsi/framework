ccce8cc878eb000c47a5b8d82ce250528961ca33
BollinaniVamsi <vamsi2techie@gmail.com>
2024-11-10 18:20:15 +0530

    testing python code

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,7 +6,9 @@ pipeline {
             steps {
                 sh '''
                 chmod 777 test.py
-                python test.py
+                python test.py | tee pdb_output.log
+                echo "printing the log file output"
+                cat pdb_output.log
                 '''
             }
         }
--- test.py
+++ test.py
@@ -1,14 +1,9 @@
 import pdb
-import logging
-
-# Set up logging to display debug info in Jenkins console
-logging.basicConfig(level=logging.DEBUG)
 
 def test_function():
     x = 10
     y = 0
-    logging.debug(f'Variables before division: x={x}, y={y}')
-    pdb.set_trace()  # or just use logging without an interactive pdb prompt
+    pdb.set_trace()  # Set breakpoint
     z = x / y
     print(z)
 
